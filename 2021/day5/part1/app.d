5565b4e8ed85b1fdb93049307b331046db091e68
João Lourenço <jlourenco5691@gmail.com>
2021-12-06 19:55:00 +0000

    2021/day5/part1: implement part1
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ 2021/day5/part1/app.d
@@ -0,0 +1,26 @@
+import std.algorithm : any, each, filter, fold, minElement, map, sort, uniq;
+import std.array : array, back, byPair, front;
+import std.range : cycle, enumerate, iota, take, transposed, walkLength, zip;
+import std.stdio : stdin, writeln;
+import std.typecons : No, Tuple, Yes;
+
+void main()
+{
+	alias Point = Tuple!(size_t, "x", size_t, "y");
+	stdin.byLine
+		.map!`a.to!string.splitter(" -> ").map!"a.splitter(',')"`
+		.map!`a.map!"a.map!(to!int)".map!array`.map!array
+		.filter!`a.dup.transposed.any!"a.uniq.walkLength == 1"`
+		.fold!((rate, pts) {
+			alias pred = (step) { with(step) return pts.front[index].iota(pts.back[index] + value, value).array; };
+			auto seq = pts.dup.transposed
+				.map!array.map!"a.back < a.front ? -1 : 1".enumerate
+				.map!pred.array;
+			immutable shortest = seq.enumerate.minElement!"a.value.walkLength".index;
+			zip(
+				shortest == 0 ? seq.front.cycle.take(seq.back.length).array : seq.front,
+				shortest == 1 ? seq.back.cycle.take(seq.front.length).array : seq.back
+			).each!((x, y) => rate[Point(x, y)]++);
+			return rate;
+		})(size_t[Point].init).byPair.filter!"a.value > 1".walkLength.writeln;
+}
